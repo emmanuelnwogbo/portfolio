a8dcd2764756b88624843cb62c91ac890673841a
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-11-17 15:59:51 +0100

    add nuxtserverinit

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,10 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  target: 'static',
+  router: {
+    base: "/portfolio/"
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
